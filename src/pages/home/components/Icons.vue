3a9e95c85dab95cbe374c21396cfee82541f8998
your name <your email>
2021-10-01 23:53:46 +0800

    add recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -80,38 +80,40 @@ export default {
         // 这里是指高度是相对于宽度百分百的百分之50
         padding-bottom :50%
         // background :green
-    .icon
-        position: relative
-        overflow :hidden
-        vertical-align :middle
-        float :left
-        width :25%
-        height :0
-        padding-bottom :25%
-        // background :red
-        border: 0
-        .icon-img
-            // text-align :center
-            position :absolute
-            top:0
-            left:0
-            right :0
-            bottom:.44rem
-            box-sizing :1rem
-            padding:.1rem
-            // background :blue
-            .icon-imgcontent
-                height :100%
-                display :block
-                margin:0 auto
-        .icon-desc
-            position :absolute
-            left:0
-            right :0
-            bottom:0
-            height :.44rem
-            line-height :.44rem
-            color :$darkTextColor
-            text-align :center
-            ellipsis()
+    .icons
+        margin-top :.1rem
+        .icon
+            position: relative
+            overflow :hidden
+            vertical-align :middle
+            float :left
+            width :25%
+            height :0
+            padding-bottom :25%
+            // background :red
+            border: 0
+            .icon-img
+                // text-align :center
+                position :absolute
+                top:0
+                left:0
+                right :0
+                bottom:.44rem
+                box-sizing :1rem
+                padding:.1rem
+                // background :blue
+                .icon-imgcontent
+                    height :100%
+                    display :block
+                    margin:0 auto
+            .icon-desc
+                position :absolute
+                left:0
+                right :0
+                bottom:0
+                height :.44rem
+                line-height :.44rem
+                color :$darkTextColor
+                text-align :center
+                ellipsis()
 </style>
